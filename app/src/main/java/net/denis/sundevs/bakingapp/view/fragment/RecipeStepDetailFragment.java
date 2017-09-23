29596cf3227293cfde1e9069efdab26be8ed92d6
mohamaddenisjs <mohamaddenisjs@gmail.com>
2017-09-23 08:35:25 +0700

    Baking App fix testing

--- app/src/main/java/net/denis/sundevs/bakingapp/view/fragment/RecipeStepDetailFragment.java
+++ app/src/main/java/net/denis/sundevs/bakingapp/view/fragment/RecipeStepDetailFragment.java
@@ -162,7 +162,7 @@ public class RecipeStepDetailFragment extends Fragment implements View.OnClickLi
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        Log.v(TAG, "save instance ");
+        Log.v(TAG, "In frag's on save instance state ");
         outState.putInt(EXTRA_STEP_NUMBER, mNumber);
         outState.putBoolean(EXTRA_STEP_FIRST, mFirst);
         outState.putBoolean(EXTRA_STEP_LAST, mLast);
